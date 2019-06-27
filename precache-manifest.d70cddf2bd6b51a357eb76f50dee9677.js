fb366c02487a4a6f85370a4755fe336d93a9079f
Antonio1505 <antoniorollande@gmail.com>
2019-06-27 13:22:04 +0300

    Updates

---
+++ precache-manifest.d70cddf2bd6b51a357eb76f50dee9677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f432152c6502a4343813eaa1a546b51c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffea37c7f4e434681906",
+    "url": "/static/css/main.79a9cb44.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "ffea37c7f4e434681906",
+    "url": "/static/js/main.74e30b26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  }
+]);
